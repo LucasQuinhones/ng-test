6b3d9f8617279bd73cdaffb0c360dfa7b0aeff9e
lucasquinhones <lucasquinhones10@gmail.com>
2024-04-09 10:07:25 -0300

    Adding a context to my exception

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -34,9 +34,9 @@ describe(UniqueIdService.name, () => {
 
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
     should throw when called with empty`, () => {
-      const emptyValues = [null, undefined, '', '0', '1'];
+      const emptyValues = [null, undefined, '', '0', '1', 'app'];
       emptyValues.forEach(emptyValue => {
-        expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow()
+        expect(() => service.generateUniqueIdWithPrefix(emptyValue)).withContext(`Empty value: ${emptyValue}`).toThrow();
       })
     });
 });
